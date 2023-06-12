a2c44e757550ec4e82c46c4994c96e24a819b1c1
ayushagarwal2901 <86055290+ayushagarwal2901@users.noreply.github.com>
2023-06-13 00:58:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Add any JavaScript code or functionality you want for your website
+
+// Example code to scroll smoothly when clicking on navigation links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
